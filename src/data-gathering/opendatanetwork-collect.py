6539f3c905f17554b0f0302a065e72c0247e08cb
Kornraphop Kawintiranon <kornraphop@Kornraphops-MacBook-Pro.local>
2017-09-29 23:21:53 -0400

    feat: collect graduation rates every places (cities)

--- src/data-gathering/opendatanetwork-collect.py
+++ src/data-gathering/opendatanetwork-collect.py
@@ -2,12 +2,16 @@
 	Author: Kornraphop Kawintiranon
 	Email : kk1155@georgetown.edu
 """
-##### Purpose 
+##### Purpose
 # - Collect data from opendatanetwork API
 # - https://www.opendatanetwork.com
 
 import json
 import requests
+import numpy as np
+import pandas as pd
+import os.path
+from states import states 	# From states.py
 
 # Globle variables
 APP_TOKEN="cQovpGcdUT1CSzgYk0KPYdAI0"
@@ -43,13 +47,107 @@ def getAreas(entity_name, area_types=[]):
 			return areas
 
 	# API failed
-	return None
+	return []
 
+
+##### Gather Education data by specific area
+def getGraduationRates(area_id, year):
+	# Setup API caller
+	url = "http://api.opendatanetwork.com/data/v1/values"
+	payload = {
+		"app_token" : APP_TOKEN,
+		"describe" : "false",
+		"format" : "",
+		"variable" : "education.graduation_rates",
+		"entity_id" : area_id,
+		"forecast" : 0,
+		"year" : year
+	}
+
+	# Response from API
+	res = requests.get(url, params=payload)
+
+	# If call API success
+	if res.status_code == 200:
+		res = res.json()["data"][1:]	# First element contains just variable names
+		return res
+	else:
+		return []
+
+
+def collectGraduationRates():
+	# Example
+	# areas = getAreas("DC", ["region.place"])
+	# for area in areas:
+	# 	print(getGraduationRates(area["id"], "2013"))
+
+	# Variables
+	graduation_rates_header = ["area_id", "area_name", "area_type", "year"]
+	graduation_rate_types = ["percent_associates_degree", "percent_bachelors_degree_or_higher", "percent_graduate_or_professional_degree", "percent_high_school_graduate_or_higher", "percent_less_than_9th_grade"]
+
+	# Delete all old data
+	graduation_rates_file_name = "graduation_rates.csv"
+	if os.path.isfile(graduation_rates_file_name):
+		os.remove(graduation_rates_file_name)
+		with open(graduation_rates_file_name, 'w') as f:
+			f.write(",".join(graduation_rates_header + graduation_rate_types) + '\n')
+
+
+	count = 0
+	graduation_rates_count = 0
+	for state in list(states.keys()):
+		# if state != "DC": continue
+		areas = getAreas(state, ["region.place"])
+		count += len(areas)
+
+		for area in areas:
+			for year in range(2013, 2018):
+				print("Gathering - State: " + state + " Area: " + area["name"] + " Year: " + str(year))
+				
+				##### Graduation Rates
+				graduation_rates = getGraduationRates(area["id"], year)
+				graduation_rates_count += len(graduation_rates)
+				if len(graduation_rates) > 0:	# Not empty
+					row = [str(area["id"]), area["name"], area["type"], str(year)]
+					rates = [None] * len(graduation_rate_types)
+					for graduation_rate in graduation_rates:
+						rate_name = graduation_rate[0]
+
+						# There are 5 graduation rate types in general
+						for idx, graduation_rate_type in enumerate(graduation_rate_types):
+							if rate_name == graduation_rate_type: rates[idx] = str(graduation_rate[1])
+				
+					# Write to file
+					with open(graduation_rates_file_name, 'a') as f:
+						row = list(map((lambda x: x.replace(",", "-")), row))	# Remove ',' in area names
+						line = ",".join(row + rates)
+						f.write(line+"\n")
+
+	# General Summary
+	print("\n########## General Summary ##########")
+	print("Place number in US: " + str(count))
+	print("Place with graduation_rates number in US: " + str(count))
 
 
 def main():
-	# Example
-	print(getAreas("DC", ["region.msa", "region.place"]))
+	collectGraduationRates()
+	
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
