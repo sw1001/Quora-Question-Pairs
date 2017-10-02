5848095760bd95cf9ec09d502a2de41e6d6349b0
Kornraphop Kawintiranon <kornraphop@Kornraphops-MacBook-Pro.local>
2017-10-01 21:49:30 -0400

    feat: collect GDP info

--- src/data-gathering/opendatanetwork-collect.py
+++ src/data-gathering/opendatanetwork-collect.py
@@ -79,12 +79,12 @@ def getGraduationRates(area_id, year):
 		return []
 
 
-def collectGraduationRates(state_abbr=[]):
+def collectGraduationRates(state_abbr=[], year_range=YEAR_RANGE):
 	# Variables
 	graduation_rates_header = ["area_id", "area_name", "area_type", "year"]
 	graduation_rate_types = ["percent_associates_degree", "percent_bachelors_degree_or_higher", "percent_graduate_or_professional_degree", "percent_high_school_graduate_or_higher", "percent_less_than_9th_grade"]
 
-	# Delete all old data
+	# Delete all old data and write header to new file
 	graduation_rates_file_name = "graduation_rates.csv"
 	with open(graduation_rates_file_name, 'w') as f:
 		f.write(",".join(graduation_rates_header + graduation_rate_types) + '\n')
@@ -98,7 +98,7 @@ def collectGraduationRates(state_abbr=[]):
 		count += len(areas)
 
 		for area in areas:
-			for year in YEAR_RANGE:
+			for year in year_range:
 				print("Gathering - State: " + state + " Area: " + area["name"] + " Year: " + str(year))
 				
 				##### Graduation Rates
@@ -150,12 +150,12 @@ def getCrimeCounts(area_id, year):
 		return []
 
 
-def collectCrimeCounts(state_abbr=[]):
+def collectCrimeCounts(state_abbr=[], year_range=YEAR_RANGE):
 	# Variables
 	crime_counts_header = ["area_id", "area_name", "area_type", "year"]
 	crime_count_types = ["Aggravated assault", "All Crimes", "Burglary", "Larceny", "Motor vehicle theft", "Murder and nonnegligent manslaughter", "Property crime", "Rape (revised definition)", "Robbery", "Violent crime"]
 
-	# Delete all old data
+	# Delete all old data and write header to new file
 	crime_counts_file_name = "crime_counts.csv"
 	with open(crime_counts_file_name, 'w') as f:
 		f.write(",".join(crime_counts_header + crime_count_types) + '\n')
@@ -168,7 +168,7 @@ def collectCrimeCounts(state_abbr=[]):
 		place_in_us_count += len(areas)
 
 		for area in areas:
-			for year in YEAR_RANGE:
+			for year in year_range:
 				print("Gathering - State: " + state + " Area: " + area["name"] + " Year: " + str(year))
 
 				##### Crime Counts
@@ -220,12 +220,12 @@ def getCrimeRates(area_id, year):
 		return []
 
 
-def collectCrimeRates(state_abbr=[]):
+def collectCrimeRates(state_abbr=[], year_range=YEAR_RANGE):
 	# Variables
 	crime_rates_header = ["area_id", "area_name", "area_type", "year"]
 	crime_rate_types = ["Aggravated assault", "All Crimes", "Burglary", "Larceny", "Motor vehicle theft", "Murder and nonnegligent manslaughter", "Property crime", "Rape (revised definition)", "Robbery", "Violent crime"]
 
-	# Delete all old data
+	# Delete all old data and write header to new file
 	crime_rates_file_name = "crime_rates.csv"
 	with open(crime_rates_file_name, 'w') as f:
 		f.write(",".join(crime_rates_header + crime_rate_types) + '\n')
@@ -238,7 +238,7 @@ def collectCrimeRates(state_abbr=[]):
 		place_in_us_count += len(areas)
 
 		for area in areas:
-			for year in YEAR_RANGE:
+			for year in year_range:
 				print("Gathering - State: " + state + " Area: " + area["name"] + " Year: " + str(year))
 
 				##### Crime Rates
@@ -290,7 +290,7 @@ def getEarningInfo(area_id, year):
 		return []
 
 
-def collectEarningInfo(state_abbr=[]):
+def collectEarningInfo(state_abbr=[], year_range=YEAR_RANGE):
 	# Variables
 	earning_info_header = ["area_id", "area_name", "area_type", "year"]
 	earning_info_types = ["female_full_time_median_earnings", "female_median_earnings", 
@@ -301,7 +301,7 @@ def collectEarningInfo(state_abbr=[]):
 		"percent_with_earnings_25000_to_34999", "percent_with_earnings_35000_to_49999", "percent_with_earnings_50000_to_64999",
 		"percent_with_earnings_65000_to_74999", "percent_with_earnings_75000_to_99999", "percent_with_earnings_over_100000"]
 
-	# Delete all old data
+	# Delete all old data and write header to new file
 	earning_info_file_name = "earning_info.csv"
 	with open(earning_info_file_name, 'w') as f:
 		f.write(",".join(earning_info_header + earning_info_types) + '\n')
@@ -314,7 +314,7 @@ def collectEarningInfo(state_abbr=[]):
 		place_in_us_count += len(areas)
 
 		for area in areas:
-			for year in YEAR_RANGE:
+			for year in year_range:
 				print("Gathering - State: " + state + " Area: " + area["name"] + " Year: " + str(year))
 
 				##### Earning Info
@@ -341,6 +341,77 @@ def collectEarningInfo(state_abbr=[]):
 	print("Place number in US: " + str(place_in_us_count))
 	print("Place with earning_info number in US: " + str(earning_info_count))
 
+
+def getGDPInfo(area_id, year):
+	# Setup API caller
+	url = "http://api.opendatanetwork.com/data/v1/values"
+	payload = {
+		"app_token" : APP_TOKEN,
+		"describe" : "false",
+		"format" : "",
+		"variable" : "economy.gdp",
+		"entity_id" : area_id,
+		"forecast" : 0,
+		"year" : year
+	}
+
+	# Response from API
+	res = requests.get(url, params=payload)
+
+	# If call API success
+	if res.status_code == 200:
+		res = res.json()["data"][1:]	# First element contains just variable names
+		return res
+	else:
+		return []
+
+
+def collecGDPInfo(state_abbr=[], year_range=YEAR_RANGE):
+	# Variables
+	gdp_info_header = ["area_id", "area_name", "area_type", "year"]
+	gdp_info_types = ["per_capita_gdp", "per_capita_gdp_percent_change"]
+
+	# Delete all old data and write header to new file
+	gdp_info_file_name = "gdp_info.csv"
+	with open(gdp_info_file_name, 'w') as f:
+		f.write(",".join(gdp_info_header + gdp_info_types) + '\n')
+
+	place_in_us_count = 0
+	gdp_info_count = 0	# How many places we found for gdp info
+	for state in list(states.keys()):
+		if len(state_abbr) > 0 and not state in state_abbr: continue	# If specify states
+		areas = getAreas(state, ["region.msa"])		# GDP appears only in Metro Area level, not city level 
+		place_in_us_count += len(areas)
+
+		for area in areas:
+			for year in year_range:
+				print("Gathering - State: " + state + " Area: " + area["name"] + " Year: " + str(year))
+
+				##### GDP Info
+				gdp_infos = getGDPInfo(area["id"], year)
+				if len(gdp_infos) > 0:	# Not empty
+					gdp_info_count += 1
+					row = [str(area["id"]), area["name"], area["type"], str(year)]
+					gdp_info_row = [""] * len(gdp_info_types)
+
+					for gdp_info in gdp_infos:
+						gdp_info_name = gdp_info[0]
+
+						for idx, gdp_info_type in enumerate(gdp_info_types):
+							if gdp_info_name == gdp_info_type: gdp_info_row[idx] = str(gdp_info[1])
+
+					# Write to file
+					with open(gdp_info_file_name, 'a') as f:
+						row = list(map((lambda x: x.replace(",", "-")), row))	# Remove ',' in area names
+						line = ",".join(row + gdp_info_row)
+						f.write(line+"\n")
+
+	# General Summary
+	print("\n########## General Summary ##########")
+	print("Place number in US: " + str(place_in_us_count))
+	print("Place with gdp_info number in US: " + str(gdp_info_count))
+
+
 def main():
 	"""
 		Specify states by using params list such as ["VA", "DC", ...]
@@ -348,9 +419,10 @@ def main():
 		Uncomment to activate methods
 	"""
 	# collectGraduationRates()
-	collectCrimeCounts()
+	# collectCrimeCounts()
 	# collectCrimeRates()				# Crime rate per 100k people
 	# collectEarningInfo()
+	collecGDPInfo(year_range=range(2001, 2014))
 
 	# Alarm when finished
 	os.system('say "your program has finished"')
