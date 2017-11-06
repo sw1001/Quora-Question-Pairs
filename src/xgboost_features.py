9fc07cd6ea33bece091af345b592144f95a78d46
WuZhuoran <munoliver007@gmail.com>
2017-11-06 17:25:07 -0500

    feat: update xgboost, add preprocessing

--- src/xgboost_features.py
+++ src/xgboost_features.py
@@ -14,13 +14,109 @@ from collections import defaultdict
 import numpy as np
 import pandas as pd
 
+import re
+from string import punctuation
 from nltk.corpus import stopwords
 import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
 from collections import Counter
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
 
 import xgboost as xgb
+
+
+def cutter(word):
+    if len(word) < 4:
+        return word
+    return WordNetLemmatizer().lemmatize(WordNetLemmatizer().lemmatize(word, "n"), "v")
+
+
+def cleanProcess(text):
+    if pd.isnull(text):
+        return ''
+
+    stops = set(stopwords.words("english"))
+    # Clean the text, with the option to remove stop_words and to stem words.
+
+    # Clean the text
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    text = re.sub(r"what's", "", text)
+    text = re.sub(r"What's", "", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"I'm", "I am", text)
+    text = re.sub(r" m ", " am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r"60k", " 60000 ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e-mail", "email", text)
+    text = re.sub(r"\s{2,}", " ", text)
+    text = re.sub(r"quikly", "quickly", text)
+    text = re.sub(r" usa ", " America ", text)
+    text = re.sub(r" USA ", " America ", text)
+    text = re.sub(r" u s ", " America ", text)
+    text = re.sub(r" uk ", " England ", text)
+    text = re.sub(r" UK ", " England ", text)
+    text = re.sub(r"india", "India", text)
+    text = re.sub(r"switzerland", "Switzerland", text)
+    text = re.sub(r"china", "China", text)
+    text = re.sub(r"chinese", "Chinese", text)
+    text = re.sub(r"imrovement", "improvement", text)
+    text = re.sub(r"intially", "initially", text)
+    text = re.sub(r"quora", "Quora", text)
+    text = re.sub(r" dms ", "direct messages ", text)
+    text = re.sub(r"demonitization", "demonetization", text)
+    text = re.sub(r"actived", "active", text)
+    text = re.sub(r"kms", " kilometers ", text)
+    text = re.sub(r"KMs", " kilometers ", text)
+    text = re.sub(r" cs ", " computer science ", text)
+    text = re.sub(r" upvotes ", " up votes ", text)
+    text = re.sub(r" iPhone ", " phone ", text)
+    text = re.sub(r"\0rs ", " rs ", text)
+    text = re.sub(r"calender", "calendar", text)
+    text = re.sub(r"ios", "operating system", text)
+    text = re.sub(r"gps", "GPS", text)
+    text = re.sub(r"gst", "GST", text)
+    text = re.sub(r"programing", "programming", text)
+    text = re.sub(r"bestfriend", "best friend", text)
+    text = re.sub(r"dna", "DNA", text)
+    text = re.sub(r"III", "3", text)
+    text = re.sub(r"the US", "America", text)
+    text = re.sub(r"Astrology", "astrology", text)
+    text = re.sub(r"Method", "method", text)
+    text = re.sub(r"Find", "find", text)
+    text = re.sub(r"banglore", "Banglore", text)
+    text = re.sub(r" J K ", " JK ", text)
+
+    text = text.lower().replace(",000,000", "m").replace(",000", "k").replace("′", "'").replace("’", "'") \
+        .replace("won't", "will not").replace("cannot", "can not").replace("can't", "can not") \
+        .replace("n't", " not").replace("what's", "what is").replace("it's", "it is") \
+        .replace("'ve", " have").replace("i'm", "i am").replace("'re", " are") \
+        .replace("he's", "he is").replace("she's", "she is").replace("'s", " own") \
+        .replace("%", " percent ").replace("₹", " rupee ").replace("$", " dollar ") \
+        .replace("€", " euro ").replace("'ll", " will").replace("=", " equal ").replace("+", " plus ")
+    text = re.sub('[“”\(\'…\)\!\^\"\.;:,\-\?？\{\}\[\]\\/\*@]', ' ', text)
+    text = re.sub(r"([0-9]+)000000", r"\1m", text)
+    text = re.sub(r"([0-9]+)000", r"\1k", text)
+    text = ' '.join([cutter(w) for w in text.split()])
+
+    # Remove punctuation from text
+    text = ''.join([c for c in text if c not in punctuation]).lower()
+
+    text = text.split()
+    text = [w for w in text if not w in stops]
+    text = ' '.join(text)
+
+    # Return a list of words
+    return (text)
 
 
 def word_match_share(row, stops=None):
@@ -233,10 +329,16 @@ def main():
     X_train_ab = X_train_ab.drop('jaccard_distance', axis=1)
 
     df_train = pd.read_csv('../input/train.csv')
-    df_train = df_train.fillna(' ')
-
     df_test = pd.read_csv('../input/test.csv')
-    ques = pd.concat([df_train[['question1', 'question2']], \
+
+    # Clean the data
+
+    df_train["question1"] = df_train["question1"].fillna("").apply(cleanProcess)
+    df_train["question2"] = df_train["question2"].fillna("").apply(cleanProcess)
+    df_test["question1"] = df_test["question1"].fillna("").apply(cleanProcess)
+    df_test["question2"] = df_test["question2"].fillna("").apply(cleanProcess)
+
+    ques = pd.concat([df_train[['question1', 'question2']],
                       df_test[['question1', 'question2']]], axis=0).reset_index(drop='index')
     q_dict = defaultdict(set)
     for i in range(ques.shape[0]):
@@ -316,7 +418,7 @@ def main():
     watchlist = [(d_train, 'train'), (d_valid, 'valid')]
 
     bst = xgb.train(params, d_train, 2500, watchlist, early_stopping_rounds=50, verbose_eval=50)
-    bst.save_model(args.save + '.mdl')
+    # bst.save_model(args.save + '.mdl')
 
     # bst = xgb.Booster({'nthread': 4})  # init model
     # bst.load_model('xgboost_features.mdl')  # load data
